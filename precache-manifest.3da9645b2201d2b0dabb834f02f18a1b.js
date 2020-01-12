89b463cf190258481fd499de8f657533f6917308
poengen <pal.oskar.engen@gmail.com>
2020-01-12 21:17:22 +0100

    Updates

---
+++ precache-manifest.3da9645b2201d2b0dabb834f02f18a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46660ca940bea3acae171d13318ffe7d",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "072d610fe3afde239511",
+    "url": "/seasons/static/css/main.436ac32e.chunk.css"
+  },
+  {
+    "revision": "ae140e4c5149dbf70bbe",
+    "url": "/seasons/static/js/2.b1010b9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/seasons/static/js/2.b1010b9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "072d610fe3afde239511",
+    "url": "/seasons/static/js/main.1544d60d.chunk.js"
+  },
+  {
+    "revision": "7e6be9a8cc2514338533",
+    "url": "/seasons/static/js/runtime-main.bf2e6898.js"
+  }
+]);
